a123016ad5ca23946ceb453bf4e705574192f12c
DESKTOP-B0LI7JC\78967 <78967132@qq.com>
2017-06-22 23:37:17 +0800

    no message

---
+++ ConfigHelper.cs
@@ -0,0 +1,91 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Helpers
+{
+    public class ConfigHelper
+    {
+        private string configpath;
+
+        public string Configpath
+        {
+            get { return configpath; }
+        }
+        private XDocument xDoc;
+
+        public XDocument XDoc
+        {
+            get { return xDoc; }
+        }
+
+        public ConfigHelper(string _configPath)
+        {
+            configpath = _configPath;
+            var file = new FileInfo(_configPath);
+            Directory.CreateDirectory(file.DirectoryName);
+            if (!File.Exists(configpath))
+            {
+                XDocument x = new XDocument(
+                new XDeclaration("1.0", "utf-8", "yes"),//添加XML文件声明
+                new XElement("Config")
+                );
+                x.Save(configpath);
+            }
+            xDoc = XDocument.Load(configpath);
+        }
+        public string Get(string type, string key,string defaultValue="")
+        {
+            using (FileStream fs = new FileStream(configpath, FileMode.Open, FileAccess.ReadWrite, FileShare.Read, 1024, false))
+            {
+                xDoc = XDocument.Load(fs);
+                var _type = xDoc.Root.Element(type);
+                if (_type == null)
+                {
+                    xDoc.Root.Add(new XElement(type));
+                    _type = xDoc.Root.Element(type);
+                }
+                var _key = _type.Element(key);
+                if (_key == null)
+                {
+                    _type.Add(new XElement(key, defaultValue));
+                    _key = _type.Element(key);
+                }
+                fs.SetLength(0);
+                xDoc.Save(fs,SaveOptions.OmitDuplicateNamespaces);
+                return _key.Value;
+            }
+        }
+        public void Set(string type, string key, string value)
+        {
+            using (FileStream fs = new FileStream(configpath, FileMode.Open, FileAccess.ReadWrite, FileShare.Read, 1024, false))
+            {
+                xDoc = XDocument.Load(fs);
+                var _type = xDoc.Root.Element(type);
+                if (_type == null)
+                {
+                    xDoc.Root.Add(new XElement(type));
+                    _type = xDoc.Root.Element(type);
+                }
+                var _key = _type.Element(key);
+                if (_key == null)
+                {
+                    _type.Add(new XElement(key, value));
+                    _key = _type.Element(key);
+                }
+                if (string.IsNullOrEmpty(value))
+                {
+                    value = "";
+                }
+                _type.Element(key).Value = value;
+                fs.SetLength(0);
+                xDoc.Save(fs);
+            }
+        }
+    }
+}
