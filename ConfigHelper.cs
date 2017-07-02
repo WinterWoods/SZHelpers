fc03ef1f7c400e57ac6c56ac20a757a5ad3c566d
EAST\east <78967132@qq.com>
2017-07-02 10:46:14 +0800

    no message

--- ConfigHelper.cs
+++ ConfigHelper.cs
@@ -1,5 +1,4 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -24,7 +23,7 @@ namespace Helpers
             get { return xDoc; }
         }
 
-        public ConfigHelper(string _configPath)
+        public ConfigHelper(string _configPath="config.txt")
         {
             configpath = _configPath;
             var file = new FileInfo(_configPath);
